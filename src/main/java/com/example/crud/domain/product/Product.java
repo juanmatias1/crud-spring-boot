2d003dfd8457673f92516a6dcca560a4e2f5b415
Juan Matias Calixto <juanmatiasca1@gmail.com>
2024-07-25 19:15:01 -0300

    feat: add column active

---
+++ src/main/java/com/example/crud/domain/product/Product.java
@@ -0,0 +1,30 @@
+package com.example.crud.domain.product;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Table(name = "product")
+@Entity(name = "product")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String name;
+
+    private Integer price_in_cents;
+
+    private Boolean active;
+
+    public Product(RequestProductDTO requestProductDTO) {
+        this.name = requestProductDTO.name();
+        this.price_in_cents = requestProductDTO.price_in_cents();
+        this.active = true;
+    }
+}
